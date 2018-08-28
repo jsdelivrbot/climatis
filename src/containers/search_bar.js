ff33e6a4a6c3e41837043520ee58063ccdbcc93f
Pedro Silva <pedro.rls93@gmail.com>
2018-08-28 16:50:00 -0300

    styled seach bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,8 +4,11 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     render(){
         return(
-            <form className='input-group'>
-                <input/>
+            <form className="input-group mb-3">
+                <input type="text"  className="form-control" placeholder="Type a city name"/>
+                <span className="input-group-btn">
+                    <button type="button" className="btn btn-secondary">Search</button>
+                </span>
             </form>
         );
     }
