0878cad43f6783699d63a13abe994770ac3a3ac6
Pedro Silva <pedro.rls93@gmail.com>
2018-08-28 19:02:30 -0300

    action to fetch wether data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,18 @@
+import axios from 'axios';
 import weather_key from '../../keys';
 
 const API_KEY = weather_key;
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},br`;
+    const request = axios.get(url);
+
+    return{
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
