dcc45a13d9400e50bd6743131355c40a2f315c82
Pedro Silva <pedro.rls93@gmail.com>
2018-08-28 18:17:27 -0300

    methods to handle request to weather api defined

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {term: ' '};
+    }
+
+    onInputChange(event){
+        this.setState({
+            term: event.target.value
+        });
+        console.log(event.target.value);
+    }
+
+    onFormSubmit(event){
+        event.preventDefault();
+    }
+
     render(){
         return(
-            <form className="input-group mb-3">
-                <input type="text"  className="form-control" placeholder="Enter a city name"/>
+            <form 
+                className="input-group mb-3"
+                onSubmit={ this.onFormSubmit }
+            >
+                <input 
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter a city name for the forecast"
+                    value={ this.state.term }
+                    onChange={ (event) => this.onInputChange(event) }
+                />
                 <span className="input-group-btn">
-                    <button type="submit" className="btn btn-secondary">Search</button>
+                    <button 
+                        type="submit"
+                        className="btn btn-secondary"
+                    >Search</button>
                 </span>
             </form>
         );
