ba424db02178b10fe2ee2e612728957ee60546e2
Pedro Silva <pedro.rls93@gmail.com>
2018-08-28 16:56:05 -0300

    small changes and fixes

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,9 +5,9 @@ class SearchBar extends Component {
     render(){
         return(
             <form className="input-group mb-3">
-                <input type="text"  className="form-control" placeholder="Type a city name"/>
+                <input type="text"  className="form-control" placeholder="Enter a city name"/>
                 <span className="input-group-btn">
-                    <button type="button" className="btn btn-secondary">Search</button>
+                    <button type="submit" className="btn btn-secondary">Search</button>
                 </span>
             </form>
         );
