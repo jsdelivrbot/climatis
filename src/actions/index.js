479fb6a5571d760b5b530da66210576794b353d3
Pedro Silva <pedro.rls93@gmail.com>
2018-08-28 22:58:06 -0300

    fixed error on api url

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { WEATHER_KEY } from '../../keys';
 
 const API_KEY = WEATHER_KEY;
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
