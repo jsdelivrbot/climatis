1d8228f920227a60d02e2083cd9f4ca388c31727
Pedro Silva <pedro.rls93@gmail.com>
2018-08-28 22:44:22 -0300

    action now returns a new array of stataes

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,8 +11,6 @@ export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},br`;
     const request = axios.get(url);
 
-    console.log('Request: ', request);
-
     return{
         type: FETCH_WEATHER,
         payload: request
