09362b963a999b6d734bb05b033405dfee222ffe
Pedro Silva <pedro.rls93@gmail.com>
2018-08-28 21:41:13 -0300

    able to fetch data through the searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 
 class SearchBar extends Component {
@@ -16,13 +19,15 @@ class SearchBar extends Component {
 
     onFormSubmit(event){
         event.preventDefault();
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render(){
         return(
             <form 
                 className="input-group mb-3"
-                onSubmit={ this.onFormSubmit }
+                onSubmit={ (event) => this.onFormSubmit(event) }
             >
                 <input 
                     type="text"
@@ -42,4 +47,9 @@ class SearchBar extends Component {
     }
 }
 
-export default SearchBar
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchWeather }, dispatch);
+};
+
+
+export default connect(null, mapDispatchToProps)(SearchBar);
