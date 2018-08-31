1b205c6f02627e0a1daa0d624ff3b21b688540db
Pedro Silva <pedro.rls93@gmail.com>
2018-08-31 16:25:54 -0300

    methods for the necessary data do generate the charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,71 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+
+    renderWeather(cityData){
+        const name = cityData.city.name;
+        const temps = cityData.list.map( (weather, idx) => {
+            return {
+                x: idx, 
+                y: weather.main.temp - 275.15,
+            }
+        });
+        const humidities = cityData.list.map( (weather, idx) => {
+            return {
+                x: idx, 
+                y: weather.main.humidity
+            }
+        });
+        const pressures = cityData.list.map( (weather, idx) => {
+            return {
+                x: idx, 
+                y: weather.main.pressure
+            }
+        });        
+
+        return(
+            <tr key={ name }>
+                <td className="row">
+                    <h5>{ name }</h5>
+                </td>
+                <td className="">
+                    <Chart width={ 180 } height={ 150 } data={ temps } unit='°C' color='red'/>
+                </td>
+                <td className="">
+                    <Chart width={ 180 } height={ 150 } data={ humidities } unit='%' color='blue'/>
+                </td>
+                <td className="">
+                    <Chart width={ 180 } height={ 150 } data={ pressures } unit='hPa' color='gray'/>
+                </td>
+            </tr>
+        );
+    }
     render(){
+
         return(
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temperature</th>
+                        <th>Humidity</th>
+                        <th>Pressure</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    { this.props.weather.map(this.renderWeather) }
                 </tbody>
             </table>
         );
     }
 }
 
-export default WeatherList;
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
